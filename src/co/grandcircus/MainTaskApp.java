c0014a8b135a66a316e731be2d75c2f9456fac09
chriskrieg <chriskrieg95@gmail.com>
2019-10-28 21:43:19 -0400

    included a way to pre-load my data for testing purposes using MySQL

--- src/co/grandcircus/MainTaskApp.java
+++ src/co/grandcircus/MainTaskApp.java
@@ -1,5 +1,9 @@
 package co.grandcircus;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -10,15 +14,39 @@ public class MainTaskApp {
 
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
+		String url = "jdbc:mysql://localhost:3306/test";
+		String user = "root";
+		String password = "PureLife1";
+		boolean completed = false;
+		String name = "";
+		String description = "";
+		String date = "";
 		List<Task> tasks = new ArrayList<>();
-		String cont = "yes";
 
+		/*
+		 * MySQL attempt to pre-load data
+		 */
+		try {
+			Connection myConn = DriverManager.getConnection(url, user, password);
+			Statement myStat = myConn.createStatement();
+			ResultSet myRs = myStat.executeQuery("select * from tasks");
+			while (myRs.next()) {
+				completed = false;
+				date = myRs.getString("Date");
+				name = myRs.getString("Name");
+				description = myRs.getString("Description");
+				tasks.add(addTask(completed, date, name, description));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		/*
+		 * Beginning of the loop
+		 */
+		String cont = "yes";
 		while (cont.toLowerCase().startsWith("y")) {
 			// your code should start here!
-			boolean completed = false;
-			String name = "";
-			String description = "";
-			String date = "";
 			printTaskMenu();
 			int userInput = Validator.getInt(scnr, "What would you like to do? ");
 				if(userInput == 1) {
@@ -46,8 +74,7 @@ public class MainTaskApp {
 					printNames(tasks);
 					String name1 = Validator.getString(scnr, "Which name do you want to search for? ");
 					sortList(name1, tasks);
-				}
-				else if(userInput == 6) {
+				}else if(userInput == 6) {
 					break;
 				}
 			System.out.println("Do you want to continue (yes/no)");
@@ -61,13 +88,12 @@ public class MainTaskApp {
 	}
 	
 	public static void printTaskMenu() {
-		
 		System.out.println("Welcome to the Task Manager!");
 		System.out.printf("\n\t1. %s", "List tasks");
 		System.out.printf("\n\t2. %s", "Add task");
 		System.out.printf("\n\t3. %s", "Delete task");
 		System.out.printf("\n\t4. %s", "Mark task complete");
-		System.out.printf("\n\t5. %s", "Search tasks by name");
+		System.out.printf("\n\t5. %s", "Filter tasks by name");
 		System.out.printf("\n\t6. %s", "Quit");
 		System.out.println("\n");
 	}
@@ -108,5 +134,39 @@ public class MainTaskApp {
 		}
 		System.out.println("");
 	}
+	/*
+	public static void filterDates(String date, List<Task> tasks) {
+		System.out.printf("%-15s %-15s %-15s %-15s %-15s\n", "Task#", "Done?", "Due Date", "Team Member", "Description");
+		int counter = 1;
+//		String[] dateArr = date.split("/");
+//		int userMonth = Integer.parseInt(dateArr[0]);
+//		int userDay = Integer.parseInt(dateArr[1]);
+//		int userYear = Integer.parseInt(dateArr[2]);
+		for(Task task : tasks) {
+			List<Date> myList = new ArrayList<>();
+			SimpleDateFormat formatter1= new SimpleDateFormat("MM/dd/yyyy");  
+			Date date1;
+			try {
+				date1 = formatter1.parse(task.getDueDate());
+				myList.add(date1);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+//			String[] dateTemp = task.getDueDate().split("/");
+//			int month = Integer.parseInt(dateTemp[0]);
+//			int day = Integer.parseInt(dateTemp[1]);
+//			int year = Integer.parseInt(dateTemp[2]);
+//			if (year < userYear) {
+//				System.out.printf("%-15s %s \n", counter++ + ".", task);
+//			} else if (month < userMonth) {
+//				System.out.printf("%-15s %s \n", counter++ + ".", task);
+//			} else if (day < userDay) {
+//				System.out.printf("%-15s %s \n", counter++ + ".", task);
+//			}
+			Collections.sort(myList);
+		}
+		System.out.println();
+	}
+	 */
 
 }
